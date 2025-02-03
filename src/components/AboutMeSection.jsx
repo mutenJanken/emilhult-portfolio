545554fee9aa7b5b8319b5184e57a708de8f660b
emil hult <Emilhult@live.com>
2025-02-03 21:27:39 +0100

    test bild

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -19,7 +19,7 @@ function AboutMeSection() {
         <div className="chat-bubble pic">
           <img
             className="profile-pic"
-            src="src/assets/Images/Mecropped.jpeg"
+            src={import.meta.env.BASE_URL + "src/assets/Images/Mecropped.jpeg"}
             alt="Bild på mig"
           />
         </div>
