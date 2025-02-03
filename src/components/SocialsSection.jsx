1704818d4a9ef6a04b9d9d929fb1b7ced98dec1b
emil hult <Emilhult@live.com>
2025-02-03 15:53:08 +0100

    Skapat react component SocialsSection

---
+++ src/components/SocialsSection.jsx
@@ -0,0 +1,28 @@
+function SocialsSection() {
+  return (
+    <section className="socials" id="socials">
+      <h2 className="h2section">Socials</h2>
+      <div className="socials-layout">
+        <a href="https://github.com/mutenJanken" className="github-button">
+          <img
+            className="github-logo"
+            src="src/assets/Images/Github logo.png"
+            alt="Bild inom knapp på github-logo"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/emil-hult-a47a87326/"
+          className="linkedin-button"
+        >
+          <img
+            className="linkedin-logo"
+            src="src/assets/Images/LinkedIn logo.png"
+            alt="Bild inom knapp på linkedin-logo"
+          />
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default SocialsSection;
