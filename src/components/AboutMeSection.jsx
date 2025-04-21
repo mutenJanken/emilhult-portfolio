df520a8d35670d796e22bbedd50c83828ff0e365
emil hult <Emilhult@live.com>
2025-04-21 14:47:59 +0200

    About me section update

--- src/components/AboutMeSection.jsx
+++ src/components/AboutMeSection.jsx
@@ -16,9 +16,7 @@ function AboutMeSection() {
               <small className="syntax-style lang vartype">{"string "} </small>
               <h2 className="h2section">aboutMe</h2>
               <small className="syntax-style lang sign">=</small>
-              <small className="syntax-style lang quotation">
-                &quot;{"Hello! My..."}&quot;
-              </small>
+              <small className="syntax-style lang quotation">&quot;{"Currently..."}&quot;</small>
               <small className="syntax-style lang sign">;</small>
             </div>
             <div className="console-writeline-container">
@@ -33,14 +31,13 @@ function AboutMeSection() {
             <p className="aboutme-p">
               {/* <span className="syntax-style lang sign">{">"}</span> */}
               {/* <span className="lang quotation">&quot;</span> */}
-              Hello! My name is Emil, I am a student in the{" "}
+              Currently a student in the{" "}
               <strong>
                 <em className="highlight-word">.NET</em>
               </strong>{" "}
-              development course at Nackademin. With a interest for data, and
-              logic, I am pursuing a career in IT where I can develop and
-              challenge my knowledge. So far during my studies, I have gained a
-              good understanding of backend development with{" "}
+              development program at Nackademin. With a knack for logic and problem-solving, I am pursuing a
+              career in IT where I can grow and challenge my knowledge. So far, I've gained experience in
+              backend development with{" "}
               <strong>
                 <em className="highlight-word">C#</em>
               </strong>
@@ -51,24 +48,44 @@ function AboutMeSection() {
                 <em className="highlight-word">{" Entity framework "}</em>and
                 <em className="highlight-word">{" ADO.NET"}</em>
               </strong>
-              . Frontend development with{" "}
+              . Combining this with{" "}
               <strong>
-                <em className="highlight-word">HTML</em>
-              </strong>
-              ,{" "}
+                <em className="highlight-word">ASP.NET</em>
+              </strong>{" "}
+              to handle frontend{" "}
               <strong>
-                <em className="highlight-word">CSS</em>
-              </strong>
-              ,{" "}
+                <em className="highlight-word">API</em>
+              </strong>{" "}
+              calls and utilizing{" "}
               <strong>
-                <em className="highlight-word">Javascript</em>
-              </strong>
-              <strong>
-                {" "}
-                and <em className="highlight-word">React</em>
-              </strong>
-              . I am looking forward to be able to apply these skills in a real
-              work enviroment in the near future.
+                <em className="highlight-word">Microsoft Azure</em>
+              </strong>{" "}
+              as cloud-based backend solution for data storage.
+              <p>
+                I've also gained experience in Frontend development using{" "}
+                <strong>
+                  <em className="highlight-word">HTML</em>
+                </strong>
+                ,{" "}
+                <strong>
+                  <em className="highlight-word">CSS</em>
+                </strong>
+                ,{" "}
+                <strong>
+                  <em className="highlight-word">Javascript</em>
+                </strong>
+                <strong>
+                  {" "}
+                  and <em className="highlight-word">React. </em>
+                </strong>
+              </p>
+              <p className="internship-period">
+                <strong> <em> Internship period: nov 2025 - may 2026.</em></strong>
+              <div className="internship-period text ">
+                Seeking an opportunity where I can grow and hopefully make some meaningful
+                contributions along the way. 
+              </div>
+              </p>
               {/* <span className="lang quotation">&quot;</span> */}
             </p>
             <div>
@@ -79,6 +96,10 @@ function AboutMeSection() {
               <span className="syntax-style lang sign">{"{"}</span>
               <span className="syntax-style lang quotation">&quot;</span>
               <span className="syntax-style lang quotation">Books</span>
+              <span className="syntax-style lang quotation">&quot;</span>
+              <span className="syntax-style lang sign">,</span>
+              <span className="syntax-style lang quotation">&quot;</span>
+              <span className="syntax-style lang quotation">Hockey</span>
               <span className="syntax-style lang quotation">&quot;</span>
               <span className="syntax-style lang sign">,</span>
               <span className="syntax-style lang quotation">&quot;</span>
